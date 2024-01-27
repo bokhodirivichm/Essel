ba1a999e51b319f9255c3b142f912a413307c347
Ma'murjon <123986183+bokhodirivichm@users.noreply.github.com>
2024-01-27 03:58:49 -0800

    Essel first product

---
+++ comment.py
@@ -0,0 +1,22 @@
+# button8 = KeyboardButton("tel", request_contact=True)
+# button9 = KeyboardButton('joylashuv', request_location=True)
+
+# inline_btn_3 = InlineKeyboardButton('𝙸𝚗𝚜𝚝𝚊𝚐𝚛𝚊𝚖✅', url='https://www.instagram.com/nurafshon.uzb/')
+# inline_kb3 = InlineKeyboardMarkup().add(inline_btn_3)
+#
+# inline_btn_4 = InlineKeyboardButton('𝚝𝚎𝚕𝚎𝚐𝚛𝚊𝚖✅', url='t.me/nurafshon_uzb')
+# inline_kb4 = InlineKeyboardMarkup().add(inline_btn_4)
+
+
+    # elif message.text == "𝚕𝚘𝚌𝚊𝚝𝚒𝚘𝚗📍":
+    #     await bot.send_photo(chat_id=message.chat.id, photo=open("nurof.jpg", "rb"), caption="𝙼𝚊𝚗𝚣𝚒𝚕📍:𝚂𝚑𝚒𝚖𝚘𝚕𝚒𝚢 𝙾𝚕𝚖𝚊𝚣𝚘𝚛 17𝙱\n☎️𝚂𝚝𝚘𝚕𝚗𝚒 𝚋𝚊𝚗𝚍 𝚚𝚒𝚕𝚒𝚜𝚑 𝚞𝚌𝚑𝚞𝚗:\n+ 99899 797 14 14 \n + 99899 373 14 14")
+    #     await bot.send_location(chat_id=message.chat.id, longitude=69.252313, latitude=41.347089)
+
+    # elif message.text == "𝙸𝚗𝚜𝚝𝚊𝚐𝚛𝚊𝚖✅":
+    #     await bot.send_photo(chat_id=message.chat.id, photo=open("instagram.jpg", "rb"), caption="",
+    #                          reply_markup=inline_kb3)
+    # elif message.text == "𝚝𝚎𝚕𝚎𝚐𝚛𝚊𝚖✅":
+    #     await bot.send_photo(chat_id=message.chat.id, photo=open("telegram.jpg", "rb"), caption="",
+    #                          reply_markup=inline_kb4)
+    #
+    #     is_active.remove(message.chat.id)
